becfe38abe4764e36c3f0f2fbe24c294c4589902
TedLin <vul3865@gmail.com>
2022-10-30 16:12:28 +0800

    init

---
+++ src/main/java/com/example/mitakehw/MitakehwApplication.java
@@ -0,0 +1,13 @@
+package com.example.mitakehw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitakehwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MitakehwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mitakehw/MitakehwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mitakehw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitakehwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
